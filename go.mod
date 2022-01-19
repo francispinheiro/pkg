27e890a13e7d51e1cd62e83e587bcbe12179ad74
Francis Pinheiro <efransys@gmail.com>
2022-01-19 04:59:22 -0300

    Alteracoes pkg

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/francispinheiro/pkg
+
+go 1.17
--- routes/private_routes.go
+++ routes/private_routes.go
@@ -6,17 +6,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// PublicRoutes func for describe group of public routes.
-func PublicRoutes(a *fiber.App) {
-	// Create routes group.
-	route := a.Group("/api/v1")
-
-	// Routes for GET method:
-	route.Get("/books", controllers.GetBooks)              // get list of all books
-	route.Get("/books/:id", controllers.GetBook)           // get one book by ID
-	route.Get("/token/new", controllers.GetNewAccessToken) // create a new access tokens
-}
-
 // PrivateRoutes func for describe group of private routes.
 func PrivateRoutes(a *fiber.App) {
 	// Create routes group
